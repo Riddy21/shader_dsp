dfa24644431c421709dea7a3a9d7dd2dc77f13ae
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2024-09-20 19:07:23 -0400

    Maximizing sound output for linear smoothing

--- tests/audio_generator_render_stage_test.cpp
+++ tests/audio_generator_render_stage_test.cpp
@@ -8,7 +8,7 @@
 #include "audio_generator_render_stage.h"
 
 TEST_CASE("AudioGeneratorRenderStage") {
-    AudioGeneratorRenderStage audio_generator(512, 44100, 2, "media/test.wav");
+    AudioGeneratorRenderStage audio_generator(512, 44100, 2, "media/sine.wav");
     AudioPlayerOutput audio_driver(512, 44100, 2);
 
     AudioRenderer & audio_renderer = AudioRenderer::get_instance();
@@ -18,12 +18,12 @@ TEST_CASE("AudioGeneratorRenderStage") {
     auto tone_param = audio_generator.find_parameter("tone");
 
     // FIXME: There is a tinyness to the sound when played at uneven intervals
-    tone_param->set_value(new float(0.8f));
+    tone_param->set_value(new float(1.0f));
 
     // Open a thread to wait few sec and the shut it down
     std::thread t1([&audio_renderer, &audio_driver, &audio_generator, &play_param](){
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        play_param->set_value(new int(5));
+        play_param->set_value(new int(1));
         std::this_thread::sleep_for(std::chrono::seconds(1));
         play_param->set_value(new int(0));
         std::this_thread::sleep_for(std::chrono::seconds(1));
