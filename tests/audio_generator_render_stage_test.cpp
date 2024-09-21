4adab3b1cae7998c62bf03627bec84a18b30ae86
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2024-09-20 23:22:58 -0400

    3 layer buffering between lines

--- tests/audio_generator_render_stage_test.cpp
+++ tests/audio_generator_render_stage_test.cpp
@@ -18,7 +18,7 @@ TEST_CASE("AudioGeneratorRenderStage") {
     auto tone_param = audio_generator.find_parameter("tone");
 
     // FIXME: There is a tinyness to the sound when played at uneven intervals
-    tone_param->set_value(new float(1.0f));
+    tone_param->set_value(new float(0.8f));
 
     // Open a thread to wait few sec and the shut it down
     std::thread t1([&audio_renderer, &audio_driver, &audio_generator, &play_param](){
