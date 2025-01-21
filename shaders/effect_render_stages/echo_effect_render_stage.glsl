42735353bd99e009d7fbf26dd9250855fcb504c1
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2025-01-20 23:13:05 -0500

    Tuning parameters

--- shaders/effect_render_stages/echo_effect_render_stage.glsl
+++ shaders/effect_render_stages/echo_effect_render_stage.glsl
@@ -12,6 +12,10 @@ void main() {
 
     for (int i = 0; i < num_echos; i++) {
         float echo_sample_index = float(i+1) * delay_increment;
+        float decay_factor = pow(decay, float(i));
+        if (echo_sample_index >= 1.0 || decay_factor < 0.001) {
+            break;
+        }
         vec2 echo_sample_coord = vec2(TexCoord.x, echo_sample_index);
         echo += texture(echo_audio_texture, echo_sample_coord) * pow(decay, float(i));
     }
