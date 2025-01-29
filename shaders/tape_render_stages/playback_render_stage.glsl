fd003243b0976c14e4df31235c3fb5d793ace231
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2025-01-28 22:39:14 -0500

    testing

--- shaders/tape_render_stages/playback_render_stage.glsl
+++ shaders/tape_render_stages/playback_render_stage.glsl
@@ -11,7 +11,7 @@ void main() {
 
         int offset = (global_time_val - time_at_start + play_position) % (playback_buffer_size.y / 2); // divide by 2 because spacing
 
-        float playback_sample_index =  (float(offset * 2) - 0.5) / float(playback_buffer_size.y);
+        float playback_sample_index =  (float(offset * 2) - 0.25) / float(playback_buffer_size.y);
 
         playback = texture(playback_texture, vec2(TexCoord.x, playback_sample_index));
     }
