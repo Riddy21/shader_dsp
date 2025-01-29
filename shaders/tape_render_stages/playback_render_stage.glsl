5801560adf0f80b95f96188b714437d6b9a6dc11
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2025-01-29 02:43:03 -0500

    small fix

--- shaders/tape_render_stages/playback_render_stage.glsl
+++ shaders/tape_render_stages/playback_render_stage.glsl
@@ -14,7 +14,6 @@ void main() {
         float playback_sample_index =  float(offset * 2 + 1) / float(playback_buffer_size.y); // For raspberry pi
         //float playback_sample_index =  (float(offset * 2) + 0.5) / float(playback_buffer_size.y);
 
-        //playback = texture(playback_texture, vec2(TexCoord.x, playback_sample_index));
         playback = texture(playback_texture, vec2(TexCoord.x, playback_sample_index));
     }
 
