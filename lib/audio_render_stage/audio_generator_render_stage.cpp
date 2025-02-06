daf988629cd33776742b499da1a18f7c3af8d862
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2025-02-05 23:35:57 -0500

    Raising sustain

--- lib/audio_render_stage/audio_generator_render_stage.cpp
+++ lib/audio_render_stage/audio_generator_render_stage.cpp
@@ -77,7 +77,7 @@ AudioGeneratorRenderStage::AudioGeneratorRenderStage(const unsigned int frames_p
     auto sustain_level_parameter =
         new AudioFloatParameter("sustain_level",
                               AudioParameter::ConnectionType::INPUT);
-    sustain_level_parameter->set_value(0.2f);
+    sustain_level_parameter->set_value(0.8f);
 
     auto release_time_parameter =
         new AudioFloatParameter("release_time",
