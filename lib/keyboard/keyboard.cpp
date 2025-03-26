e63c89d21e41da3e188626ad11789003e6b73227
Ridvan <ridvan@untether.ai>
2025-03-26 15:16:35 -0400

    Adding new multinote shader stages to main.cpp

--- lib/keyboard/keyboard.cpp
+++ lib/keyboard/keyboard.cpp
@@ -4,29 +4,23 @@
 #include "audio_core/audio_renderer.h"
 #include "audio_render_stage/audio_generator_render_stage.h"
 #include "keyboard/keyboard.h"
-#include "keyboard/piano.h"
 
 Keyboard * Keyboard::instance = nullptr;
 
-Keyboard::Keyboard() : m_piano(10) {
+Keyboard::Keyboard() {
 
 }
 
 Keyboard::~Keyboard() {}
 
 void Keyboard::key_down_callback(unsigned char key, int x, int y) {
-    if (Piano::KEY_TONE_MAPPING.find(key) != Piano::KEY_TONE_MAPPING.end()) {
-        // TODO: Change this volume key
-        instance->m_piano.key_down(Piano::KEY_TONE_MAPPING.at(key), .2f);
-    } else if (instance->m_keys.find(key) != instance->m_keys.end()) {
+    if (instance->m_keys.find(key) != instance->m_keys.end()) {
         instance->m_keys[key]->key_down();
     }
 }
 
 void Keyboard::key_up_callback(unsigned char key, int x, int y) {
-    if (Piano::KEY_TONE_MAPPING.find(key) != Piano::KEY_TONE_MAPPING.end()) {
-        instance->m_piano.key_up(Piano::KEY_TONE_MAPPING.at(key));
-    } else if (instance->m_keys.find(key) != instance->m_keys.end()) {
+    if (instance->m_keys.find(key) != instance->m_keys.end()) {
         instance->m_keys[key]->key_up();
     }
 }
